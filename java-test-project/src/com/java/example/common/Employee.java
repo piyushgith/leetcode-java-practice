a923685ee62d6e1dedec543ff85dd435eb619a6b
piyushprasad <piyushprasad2007@gmail.com>
2025-10-10 00:51:02 +0530

    first commit

---
+++ java-test-project/src/com/java/example/common/Employee.java
@@ -0,0 +1,68 @@
+package com.java.example.common;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee implements Serializable {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Employee(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+        return getId() == employee.getId()
+                && getAge() == employee.getAge()
+                && Objects.equals(getName(), employee.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + Objects.hashCode(getName());
+        result = 31 * result + getAge();
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ java-test-project/src/com/java/example/common/Product.java
@@ -0,0 +1,80 @@
+package com.java.example.common;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Product implements Serializable {
+    private int id;
+    private String name;
+    private int quantity;
+    private String description;
+
+    public Product(int id, String name, int quantity, String description) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return getId() == product.getId()
+                && getQuantity() == product.getQuantity()
+                && Objects.equals(getName(), product.getName())
+                && Objects.equals(getDescription(), product.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + Objects.hashCode(getName());
+        result = 31 * result + getQuantity();
+        result = 31 * result + Objects.hashCode(getDescription());
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
